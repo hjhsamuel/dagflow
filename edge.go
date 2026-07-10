dbad47bbb25b76b566f5b7c6d6ea8af1f36ebbea
hjhsamuel <hjh_samuel@163.com>
2026-07-10 13:27:51 +0800

    feat: add function of Edge

--- edge.go
+++ edge.go
@@ -8,6 +8,18 @@ type Edge struct {
 	f    EdgeFunc
 }
 
+func (e *Edge) From() string {
+	return e.from
+}
+
+func (e *Edge) To() string {
+	return e.to
+}
+
+func (e *Edge) Func() EdgeFunc {
+	return e.f
+}
+
 func (e *Edge) Do(message map[string]any) (map[string]any, bool) {
 	if e.f != nil {
 		return e.f(message)
